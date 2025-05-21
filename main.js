3b099b9731bfdaa1953d5b5d8064748fc28ecf81
itsryan01 <ryanagres01@gmail.com>
2025-05-21 23:13:35 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings:["Basketball player sometimes", "Tiktokerist", "Humble ferson", "Multi talented (eme)",],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
